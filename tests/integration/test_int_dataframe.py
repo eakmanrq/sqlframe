c2ad911754b8e25a9681d568271e3d33d0d75128
Ryan Eakman <6326532+eakmanrq@users.noreply.github.com>
2024-06-06 19:05:21 -0700

    fix: select after join detect all possible columns (#58)

--- tests/integration/test_int_dataframe.py
+++ tests/integration/test_int_dataframe.py
@@ -375,7 +375,7 @@ def test_join_inner(
         pyspark_employee["fname"],
         F.col("lname"),
         F.col("age"),
-        F.col("store_id").alias("renamed_store_id"),
+        F.coalesce("store_id", "age").alias("store_id_size"),
         pyspark_store.store_name,
         pyspark_store["num_sales"],
     )
@@ -384,7 +384,7 @@ def test_join_inner(
         employee["fname"],
         SF.col("lname"),
         SF.col("age"),
-        SF.col("store_id").alias("renamed_store_id"),
+        SF.coalesce("store_id", "age").alias("store_id_size"),
         store.store_name,
         store["num_sales"],
     )
