34ac1564a6ce612b91b0d3389bd606ebdab8b2bb
Juan David Barreto <zerodarkzone@gmail.com>
2025-01-07 17:53:34 -0500

    Fixed duplicate function

--- sqlframe/databricks/session.py
+++ sqlframe/databricks/session.py
@@ -66,17 +66,6 @@ class DatabricksSession(
     def _is_databricks(self) -> bool:
         return True
 
-    @classmethod
-    def _try_get_map(cls, value: t.Any) -> t.Optional[t.Dict[str, t.Any]]:
-        if (
-            value
-            and isinstance(value, list)
-            and all(isinstance(item, tuple) for item in value)
-            and all(len(item) == 2 for item in value)
-        ):
-            return dict(value)
-        return None
-
     class Builder(_BaseSession.Builder):
         DEFAULT_EXECUTION_DIALECT = "databricks"
 
