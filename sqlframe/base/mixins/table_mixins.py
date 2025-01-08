27606755a7cc6c098727699cb958fe3434465050
Juan David Barreto <zerodarkzone@gmail.com>
2025-01-08 14:09:11 -0500

    Extract duplicated code as functions

--- sqlframe/base/mixins/table_mixins.py
+++ sqlframe/base/mixins/table_mixins.py
@@ -45,18 +45,14 @@ def ensure_cte() -> t.Callable[[t.Callable], t.Callable]:
     return decorator
 
 
-class UpdateSupportMixin(_BaseTable, t.Generic[DF]):
-    @ensure_cte()
-    def update(
-        self,
-        set_: t.Dict[t.Union[Column, str], t.Union[Column, "ColumnOrLiteral", exp.Expression]],
-        where: t.Optional[t.Union[Column, str, bool]] = None,
-    ) -> LazyExpression:
+class _BaseTableMixins(_BaseTable, t.Generic[DF]):
+    def _ensure_where_condition(
+        self, where: t.Optional[t.Union[Column, str, bool]] = None
+    ) -> exp.Expression:
         self_name = self.expression.ctes[0].this.args["from"].this.alias_or_name
-        self_expr = self.expression.ctes[0].this.args["from"].this
 
         if where is None:
-            logger.warning("Empty value for `where`clause.")
+            logger.warning("Empty value for `where`clause. Defaults to `True`.")
             condition: exp.Expression = exp.Boolean(this=True)
         else:
             condition_list = self._ensure_and_normalize_cols(where, self.expression)
@@ -66,7 +62,19 @@ class UpdateSupportMixin(_BaseTable, t.Generic[DF]):
                 if col_expr.table == self.expression.args["from"].this.alias_or_name:
                     col_expr.set("table", exp.to_identifier(self_name))
             condition = condition_list[0].expression
+        return condition
 
+
+class UpdateSupportMixin(_BaseTableMixins, t.Generic[DF]):
+    @ensure_cte()
+    def update(
+        self,
+        set_: t.Dict[t.Union[Column, str], t.Union[Column, "ColumnOrLiteral", exp.Expression]],
+        where: t.Optional[t.Union[Column, str, bool]] = None,
+    ) -> LazyExpression:
+        self_expr = self.expression.ctes[0].this.args["from"].this
+
+        condition = self._ensure_where_condition(where)
         update_set = self._ensure_and_normalize_update_set(set_)
         update_expr = exp.Update(
             this=self_expr,
@@ -108,27 +116,15 @@ class UpdateSupportMixin(_BaseTable, t.Generic[DF]):
         return update_set
 
 
-class DeleteSupportMixin(_BaseTable, t.Generic[DF]):
+class DeleteSupportMixin(_BaseTableMixins, t.Generic[DF]):
     @ensure_cte()
     def delete(
         self,
         where: t.Optional[t.Union[Column, str, bool]] = None,
     ) -> LazyExpression:
-        self_name = self.expression.ctes[0].this.args["from"].this.alias_or_name
         self_expr = self.expression.ctes[0].this.args["from"].this
 
-        if where is None:
-            logger.warning("Empty value for `where`clause. defaults to `True`.")
-            condition: exp.Expression = exp.Boolean(this=True)
-        else:
-            condition_list = self._ensure_and_normalize_cols(where, self.expression)
-            if len(condition_list) > 1:
-                condition_list = [functools.reduce(lambda x, y: x & y, condition_list)]
-            for col_expr in condition_list[0].expression.find_all(exp.Column):
-                if col_expr.table == self.expression.args["from"].this.alias_or_name:
-                    col_expr.set("table", exp.to_identifier(self_name))
-            condition = condition_list[0].expression
-
+        condition = self._ensure_where_condition(where)
         delete_expr = exp.Delete(
             this=self_expr,
             where=exp.Where(this=condition),
@@ -276,23 +272,9 @@ class MergeSupportMixin(_BaseTable, t.Generic[DF]):
             self.expression, other_df.expression.copy().ctes
         )
         condition = self._ensure_and_normalize_cols(condition, self.expression)
-
-        if self.branch_id == other_df.branch_id:
-            other_df_unique_uuids = other_df.known_uuids - self.known_uuids
-            for col in condition:
-                for col_expr in col.expression.find_all(exp.Column):
-                    if (
-                        "join_on_uuid" in col_expr.meta
-                        and col_expr.meta["join_on_uuid"] in other_df_unique_uuids
-                    ):
-                        col_expr.set("table", exp.to_identifier(other_df.latest_cte_name))
+        self._handle_self_join(other_df, condition)
 
         if isinstance(condition[0].expression, exp.Column) and not clause:
-            """
-            Unique characteristics of join on column names only:
-            * The column names are put at the front of the select list
-            * The column names are deduplicated across the entire select list and only the column names (other dups are allowed)
-            """
             table_names = [
                 table.alias_or_name
                 for table in [
@@ -300,44 +282,12 @@ class MergeSupportMixin(_BaseTable, t.Generic[DF]):
                     other_df.expression.args["from"].this,
                 ]
             ]
-            potential_ctes = [
-                cte
-                for cte in join_expression.ctes
-                if cte.alias_or_name in table_names
-                and cte.alias_or_name != other_df.latest_cte_name
-            ]
-            # Determine the table to reference for the left side of the join by checking each of the left side
-            # tables and see if they have the column being referenced.
-            join_column_pairs = []
-            for join_column in condition:
-                num_matching_ctes = 0
-                for cte in potential_ctes:
-                    if join_column.alias_or_name in cte.this.named_selects:
-                        left_column = join_column.copy().set_table_name(cte.alias_or_name)
-                        right_column = join_column.copy().set_table_name(other_df.latest_cte_name)
-                        join_column_pairs.append((left_column, right_column))
-                        num_matching_ctes += 1
-                        # We only want to match one table to the column and that should be matched left -> right
-                        # so we break after the first match
-                        break
-                if num_matching_ctes == 0:
-                    raise ValueError(
-                        f"Column `{join_column.alias_or_name}` does not exist in any of the tables."
-                    )
-            join_clause = functools.reduce(
-                lambda x, y: x & y,
-                [left_column == right_column for left_column, right_column in join_column_pairs],
+
+            join_column_pairs, join_clause = self._handle_join_column_names_only(
+                condition, join_expression, other_df, table_names
             )
         else:
-            """
-            Unique characteristics of join on expressions:
-            * There is no deduplication of the results.
-            * The left join dataframe columns go first and right come after. No sort preference is given to join columns
-            """
-            condition = self._ensure_and_normalize_cols(condition, join_expression)
-            if len(condition) > 1:
-                condition = [functools.reduce(lambda x, y: x & y, condition)]
-            join_clause = condition[0]
+            join_clause = self._normalize_join_clause(condition, join_expression)
         return join_clause
 
     def _ensure_and_normalize_assignments(
