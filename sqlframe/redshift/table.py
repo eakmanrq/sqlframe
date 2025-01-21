789903502fb297fcc0551339769c2387726da565
Juan Barreto <jdbarretop@unal.edu.co>
2025-01-21 16:26:17 -0500

    Remove merge from redshift. The redshift merge statement is quite limited compared to the other engines.

--- sqlframe/redshift/table.py
+++ sqlframe/redshift/table.py
@@ -1,13 +1,8 @@
 from sqlframe.base.mixins.table_mixins import (
     DeleteSupportMixin,
-    MergeSupportMixin,
     UpdateSupportMixin,
 )
-from sqlframe.base.table import (
-    WhenMatched,
-    WhenNotMatched,
-    _BaseTable,
-)
+from sqlframe.base.table import _BaseTable
 from sqlframe.redshift.dataframe import RedshiftDataFrame
 
 
@@ -15,9 +10,6 @@ class RedshiftTable(
     RedshiftDataFrame,
     UpdateSupportMixin["RedshiftDataFrame"],
     DeleteSupportMixin["RedshiftDataFrame"],
-    MergeSupportMixin["RedshiftDataFrame"],
     _BaseTable["RedshiftDataFrame"],
 ):
     _df = RedshiftDataFrame
-    _merge_supported_clauses = [WhenMatched, WhenNotMatched]
-    _merge_support_star = False
