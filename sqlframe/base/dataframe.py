d0ad7f43da0e6ae3262e6a0c3fee8ac57deca5ec
Ryan Eakman <6326532+eakmanrq@users.noreply.github.com>
2025-05-11 14:05:38 -0700

    fix: revert repr change (#395)

--- sqlframe/base/dataframe.py
+++ sqlframe/base/dataframe.py
@@ -260,10 +260,6 @@ class BaseDataFrame(t.Generic[SESSION, WRITER, NA, STAT, GROUP_DATA]):
     def __copy__(self):
         return self.copy()
 
-    def __repr__(self) -> str:
-        fields = [f"{field.name}: {field.dataType}" for field in self.schema]
-        return "DataFrame[" + ", ".join(fields) + "]"
-
     def _display_(self) -> str:
         return self.__repr__()
 
